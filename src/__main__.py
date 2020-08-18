fb5b81c0b2c5b598eb371ec33f101829cc3a31a8
eJi <veryeji@gmail.com>
2020-08-17 21:37:48 -0400

    Implement file checking
    Files will actually be checked for proper content. Currently, the YAML file just needs to have keys for each Discord event. I will have to add in more checks for things like guild visibility and guild variables and all of that stuff.

--- src/__main__.py
+++ src/__main__.py
@@ -1,32 +1,53 @@
 import os
 
 from discord.ext import commands
+from yaml import safe_load
 
 from utils.embed import RestrictedEmbed
 
 client = commands.Bot(command_prefix='!')
+
+events = {'typing', 'message', 'message_delete', 'bulk_message_delete', 'message_edit',
+          'reaction_add', 'reaction_remove', 'channel_create', 'channel_delete', 'channel_update',
+          'pin_update', 'integrations_update', 'webhooks_update', 'member_join', 'member_leave',
+          'member_update', 'guild_update', 'role_create', 'role_delete', 'role_update',
+          'emoji_update', 'voice_state_update', 'member_ban', 'member_unban', 'invite_create',
+          'invite_delete'}
 
 
 @client.command()
 async def configure(ctx: commands.Context):
     if ctx.guild:
         if not ctx.channel.permissions_for(ctx.author).manage_guild:
+            await ctx.message.add_reaction('💢')
             return await RestrictedEmbed(ctx).fail(
                 'Manage Guild permissions are required for configuration.')
     if len(ctx.message.attachments) == 0:
         await ctx.message.add_reaction('💢')
         return await RestrictedEmbed(ctx).fail(
-            'Please attach a text file in order to configure the bot. '
+            'Please attach a YAML file in order to configure the bot. '
             'See the documentation for more information.')
-
     # TODO: add in logic for detecting syntax errors
-
-    await ctx.message.add_reaction('✅')
-    await RestrictedEmbed(ctx).unfail('File saved.')
     if ctx.guild:
         await ctx.message.attachments[0].save(f'../data/g{ctx.guild.id}.vorpal')
     else:
         await ctx.message.attachments[0].save(f'../data/u{ctx.author.id}.vorpal')
+
+    if ctx.guild:
+        with open(f'../data/g{ctx.guild.id}.vorpal', 'r') as f:
+            temp = safe_load(f)
+    else:
+        with open(f'../data/u{ctx.author.id}.vorpal', 'r') as f:
+            temp = safe_load(f)
+
+    if not all(event in temp for event in events):
+        await ctx.message.add_reaction('💢')
+        return await RestrictedEmbed(ctx).fail(
+            'This YAML file does not match the format specified in the documentation. '
+            'See the documentation for more information.')
+
+    await ctx.message.add_reaction('✅')
+    await RestrictedEmbed(ctx).unfail('File saved.')
 
 
 token = os.environ.get('TOKEN')
