b6743f6c1120e6e997ce58e09735fa930d30612b
eJi <veryeji@gmail.com>
2020-08-20 19:01:45 -0400

    Remove YAML
    Rather than using YAML, I'm opting to use an in-house language to be built in Python with SLY. It'll closely resemble Python, with a few tweaks to make configuration much easier.

--- src/__main__.py
+++ src/__main__.py
@@ -2,17 +2,8 @@ import os
 
 from discord.ext import commands
 from utils.embed import RestrictedEmbed
-from yaml import safe_load
 
 client = commands.Bot(command_prefix='|')
-
-events = {'typing', 'message', 'message_delete', 'bulk_message_delete', 'message_edit',
-          'reaction_add', 'reaction_remove', 'channel_create', 'channel_delete', 'channel_update',
-          'pin_update', 'integrations_update', 'webhooks_update', 'member_join', 'member_leave',
-          'member_update', 'guild_update', 'role_create', 'role_delete', 'role_update',
-          'emoji_update', 'voice_state_update', 'member_ban', 'member_unban', 'invite_create',
-          'invite_delete'}
-
 
 @client.command()
 async def configure(ctx: commands.Context):
@@ -24,26 +15,13 @@ async def configure(ctx: commands.Context):
     if len(ctx.message.attachments) == 0:
         await ctx.message.add_reaction('💢')
         return await RestrictedEmbed(ctx).fail(
-            'Please attach a YAML file in order to configure the bot. '
+            'Please attach a Vorpal config file in order to configure the bot. '
             'See the documentation for more information.')
     # TODO: add in logic for detecting syntax errors
     if ctx.guild:
         await ctx.message.attachments[0].save(f'../data/g{ctx.guild.id}.vorpal')
     else:
         await ctx.message.attachments[0].save(f'../data/u{ctx.author.id}.vorpal')
-
-    if ctx.guild:
-        with open(f'../data/g{ctx.guild.id}.vorpal', 'r') as f:
-            temp = safe_load(f)
-    else:
-        with open(f'../data/u{ctx.author.id}.vorpal', 'r') as f:
-            temp = safe_load(f)
-
-    if not all(event in temp for event in events):
-        await ctx.message.add_reaction('💢')
-        return await RestrictedEmbed(ctx).fail(
-            'This YAML file does not match the format specified in the documentation. '
-            'See the documentation for more information.')
 
     await ctx.message.add_reaction('✅')
     await RestrictedEmbed(ctx).unfail('File saved.')
