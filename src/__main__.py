ed1ebbf3c7f5048b0a1cb05642a167bd8f0fb209
eJi <veryeji@gmail.com>
2020-08-21 10:45:28 -0400

    Update toggle to use MongoDB
    Toggle settings would ordinarily reset upon bot restarting. Now, they will persist, which should be a better experience for those with technical difficulties. User toggles are stored with 'u{id}', and guild toggles are stored with 'g{id}'.

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,13 @@
 import os
 
+import pymongo
 from discord.ext import commands
 from utils.embed import RestrictedEmbed
 
 client = commands.Bot(command_prefix='|')
-toggles = dict()  # TODO: put this in a db
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["vorpal"]
+toggles = mydb["toggles"]
 
 
 @client.event
@@ -14,7 +17,6 @@ async def on_ready():
 
 @client.command()
 async def configure(ctx: commands.Context):
-    print('hello')
     if ctx.guild:
         if not ctx.channel.permissions_for(ctx.author).manage_guild:
             await ctx.message.add_reaction('💢')
@@ -30,12 +32,12 @@ async def configure(ctx: commands.Context):
     # TODO: add in logic for detecting syntax errors
     if ctx.guild:
         await ctx.message.attachments[0].save(f'../data/g{ctx.guild.id}.vorpal')
-        if not f'g{ctx.guild.id}' in toggles:
-            toggles[f'g{ctx.guild.id}'] = True
+        if toggles.find_one({'id': f'g{ctx.guild.id}'}) is not None:
+            toggles.insert_one({'id': f'g{ctx.guild.id}', 'toggle': True})
     else:
         await ctx.message.attachments[0].save(f'../data/u{ctx.author.id}.vorpal')
-        if not f'u{ctx.author.id}' in toggles:
-            toggles[f'u{ctx.author.id}'] = True
+        if toggles.find_one({'id': f'u{ctx.author.id}'}) is not None:
+            toggles.insert_one({'id': f'u{ctx.author.id}', 'toggle': True})
 
     await ctx.message.add_reaction('✅')
     await RestrictedEmbed(ctx).unfail('Configuration Passed', 'File saved.')
@@ -49,26 +51,31 @@ async def toggle(ctx: commands.Context):
             return await RestrictedEmbed(ctx).fail(
                 'Toggle Failed',
                 'Manage Guild permissions are required to toggle.')
-        if not f'g{ctx.guild.id}' in toggles:
-            toggles[f'g{ctx.guild.id}'] = True
-        toggles[f'g{ctx.guild.id}'] = not toggles[f'g{ctx.guild.id}']
+        if (value := toggles.find_one({'id': f'g{ctx.guild.id}'})) is not None:
+            toggles.replace_one({'id': f'g{ctx.guild.id}'},
+                                {'id': f'g{ctx.guild.id}',
+                                 'toggle': not value['toggle']})
+        else:
+            toggles.insert_one((value := {'id': f'g{ctx.guild.id}', 'toggle': False}))
         await ctx.message.add_reaction('✅')
         await RestrictedEmbed(ctx).unfail(
             'Toggle Succeeded',
             'The bot will now {}follow custom commands.'.format(
-                '' if toggles[f'g{ctx.guild.id}'] else 'not '))
+                '' if value['toggle'] else 'not '))
     else:
-        if not f'u{ctx.author.id}' in toggles:
-            toggles[f'u{ctx.author.id}'] = True
-        toggles[f'u{ctx.author.id}'] = not toggles[f'u{ctx.author.id}']
+        if (value := toggles.find_one({'id': f'u{ctx.author.id}'})) is not None:
+            toggles.replace_one({'id': f'u{ctx.author.id}'},
+                                {'id': f'u{ctx.author.id}',
+                                 'toggle': not value['toggle']})
+        else:
+            toggles.insert_one((value := {'id': f'u{ctx.author.id}', 'toggle': False}))
         await ctx.message.add_reaction('✅')
         await RestrictedEmbed(ctx).unfail(
             'Toggle Succeeded',
             'The bot will now {}follow custom commands.'.format(
-                '' if toggles[f'u{ctx.author.id}'] else 'not '))
+                '' if value['toggle'] else 'not '))
 
 
 if __name__ == '__main__':
     token = os.environ.get('TOKEN')
     client.run(token)
-    print('hello')
